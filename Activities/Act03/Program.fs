f36be15979041212813059ba438c3775d973045e
Binu-Getachew <biniammena@gmail.com>
2026-03-16 09:42:57 +0900

    Implement GCD function using Euclidean algorithm
    Modified the gcd function in Activities/Act03/Program.fs to compute the greatest common divisor of two integers recursively.

--- Activities/Act03/Program.fs
+++ Activities/Act03/Program.fs
@@ -2,8 +2,9 @@
 
 /// Modify the function `gcd` in such a way that the function computes the
 /// greatest common divisor of two 32-bit integers.
-let gcd a b =
-  0
+let rec gcd a b =
+    if b = 0 then a
+    else gcd b (a % b)
 
 gcd 360 210
 |> printfn "%d"
