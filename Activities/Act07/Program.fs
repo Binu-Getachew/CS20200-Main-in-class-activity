70431bb7b46828f59b5ba2a6ff7e9dd218b6abf1
Biniam Getachew Mena <biniammena@gmail.com>
2026-03-25 09:31:40 +0900

    Update Program.fs

--- Activities/Act07/Program.fs
+++ Activities/Act07/Program.fs
@@ -1,10 +1,10 @@
-﻿/// KAIST CS220 In-Class Activity Project
+/// KAIST CS220 In-Class Activity Project
 
 /// Problem 1: this is currently an integer list. Modify this type to be a
 /// generic list type.
-type MyList =
+type MyList<'a> =
   | Nil
-  | Cons of int * MyList
+  | Cons of 'a * MyList<'a>
 
 let empty =
   Nil
@@ -13,47 +13,62 @@ let cons elt lst =
   Cons (elt, lst)
 
 let car lst =
-  failwith "Implement"
+  match lst with
+  | Nil -> failwith "Empty list"
+  | Cons (h, _) -> h
 
 let cdr lst =
-  failwith "Implement"
+  match lst with
+  | Nil -> failwith "Empty list"
+  | Cons (_, t) -> t
 
 let (^+^) = cons
 
 let intList = 1 ^+^ 2 ^+^ 3 ^+^ empty
-// let stringList = "a" ^+^ "B" ^+^ "c" ^+^ empty
+let stringList = "a" ^+^ "B" ^+^ "c" ^+^ empty
 
 printfn "%A" <| intList
 printfn "%d" <| car intList
 printfn "%A" <| cdr intList
-// printfn "%A" <| stringList
-// printfn "%A" <| car stringList
-// printfn "%A" <| cdr stringList
+printfn "%A" <| stringList
+printfn "%A" <| car stringList
+printfn "%A" <| cdr stringList
 
 /// Problem 2: this function returns the length of the given list.
-let length lst =
-  failwith "Implement"
+let rec length lst =
+  match lst with
+  | Nil -> 0
+  | Cons (_, t) -> 1 + length t
 
-// printfn "%d" <| length lst
+printfn "%d" <| length intList
+printfn "%d" <| length stringList
 
 /// Problem 3: this function checks if the given element is included in the
 /// given list.
-let isMember elm lst =
-  failwith "Implement"
+let rec isMember elm lst =
+  match lst with
+  | Nil -> false
+  | Cons (h, t) -> if h = elm then true else isMember elm t
 
-// printfn "is X a member? %b" <| isMember "X" stringList
-// printfn "is B a member? %b" <| isMember "B" stringList
+printfn "is X a member? %b" <| isMember "X" stringList
+printfn "is B a member? %b" <| isMember "B" stringList
 
 /// Problem 4: this function joins two lists.
-let append lst1 lst2 =
-  failwith "Implement"
+let rec append lst1 lst2 =
+  match lst1 with
+  | Nil -> lst2
+  | Cons (h, t) -> Cons (h, append t lst2)
 
 let lst1 = 1 ^+^ 2 ^+^ empty
 let lst2 = 3 ^+^ 4 ^+^ empty
-// printfn "%A" <| append lst1 lst2
+printfn "%A" <| append lst1 lst2
 
 /// Problem 5: this function returns a reversed list.
 let rev lst =
-  failwith "Implement"
+  let rec loop acc lst =
+    match lst with
+    | Nil -> acc
+    | Cons (h, t) -> loop (Cons (h, acc)) t
+  loop Nil lst
 
-// printfn "%A" <| rev lst1
+printfn "%A" <| rev lst1
