94803f36496d3fd8a9b3d5d9eeb5a256ed21a409
Biniam Getachew Mena <biniammena@gmail.com>
2026-04-09 20:27:25 +0900

    Update Program.fs

--- Activities/Act01/Program.fs
+++ Activities/Act01/Program.fs
@@ -1,9 +1,14 @@
 ﻿/// KAIST CS220 In-Class Activity Project
 
+//Activity 01
+
 /// Modify the function `square` in such a way that the function takes in an
 /// integer and returns the square of the integer.
 let square n =
-  0
+  n*n
 
-square 42
-|> printfn "%d"
+let n=42
+let result=square n
+printfn "Square of %d is %d" n result
+
+//Activity 02
