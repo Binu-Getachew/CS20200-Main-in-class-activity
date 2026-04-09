829aeefa7993697e05fe718c4c349a5278476d8c
Biniam Getachew Mena <biniammena@gmail.com>
2026-04-09 20:51:21 +0900

    Update Program.fs

--- Activities/Act02/Program.fs
+++ Activities/Act02/Program.fs
@@ -3,7 +3,8 @@
 /// Modify the function `max` in such a way that the function takes in four
 /// 32-bit integers, and returns the largest number as output.
 let max a b c d =
-  0
+  max (max a b) (max c d)
 
-max -42 42 0 -1
-|> printfn "%d"
+let result2= max -42 42 0 -1
+printfn "The maximum is %d" result2
+
